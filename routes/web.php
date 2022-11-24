867ab4a0c08fe247da5d0e4937e51b9afcf91fc6
Lukadlim <99069584+Lukadlim@users.noreply.github.com>
2022-11-25 06:59:13 +0900

    update

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('test', [ProductController::class, 'index'])->name('product.index');
-
-Route::get('/product/{id?}', [ProductController::class, 'show'])->name('product.show');
+Route::resource('products', ProductController::class);
